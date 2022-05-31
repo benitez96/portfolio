07bd20627b814d5e2f645f52ef0f9f0d3bc77019
Dan <danii-benitez@hotmail.com>
2022-05-31 20:05:03 -0300

    [add] cdns

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import './input.css'
-import '../dist/output.css'
 import {App} from './App.jsx';
 
 const root = document.getElementById('root')
