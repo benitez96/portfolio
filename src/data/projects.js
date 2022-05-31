8e844dc564b97b140b671b6a85bd2c36b01bcf06
Dan <danii-benitez@hotmail.com>
2022-05-31 20:11:11 -0300

    [mod] base config

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects =[
   {
     id: 1,
     title: 'Tic Tac Toe',
-    image: new URL("../assets/tictactoe.png", import.meta.url),
+    image: new URL("./assets/tictactoe.png", import.meta.url),
     description: 'A simple tic tac toe game built with React',
     github:'https://github.com/benitez96/tictactoe',
     url:'https://benitez96.github.io/tictactoe',
@@ -10,7 +10,7 @@ export const projects =[
   {
     id: 2,
     title: 'Fakedle',
-    image: new URL("../assets/fakedle.png", import.meta.url),
+    image: new URL("./assets/fakedle.png", import.meta.url),
     description: 'A clone of Wordle built with React',
     github:'https://github.com/benitez96/fakedle',
     url:'https://benitez96.github.io/fakedle',
