3fef0f146300410ae7bd55b5dc2d5aab9290d777
Dan <danii-benitez@hotmail.com>
2022-05-31 20:20:57 -0300

    [mod] build

--- src/data/technologies.js
+++ src/data/technologies.js
@@ -3,31 +3,31 @@ export const technologies = [
   {
     title: "React",
     started_at: "2021-08-01",
-    image: new URL("./assets/reactlogo.png", import.meta.url),
+    image: new URL("../assets/reactlogo.png", import.meta.url),
     key: 1 
   },
   {
     title: "HTML",
     started_at: "2020-04-01",
-    image: new URL("./assets/htmllogo.png", import.meta.url),
+    image: new URL("../assets/htmllogo.png", import.meta.url),
     key: 2 
   },
   {
     title: "CSS",
     started_at: "2020-05-01",
-    image: new URL("./assets/css3logo.png", import.meta.url),
+    image: new URL("../assets/css3logo.png", import.meta.url),
     key: 3 
   },
   {
     title: "Javascript",
     started_at: '2020-06-01',
-    image: new URL("./assets/jslogo.png", import.meta.url),
+    image: new URL("../assets/jslogo.png", import.meta.url),
     key: 4 
   },
   {
     title: "Python",
     started_at: "2019-10-01",
-    image: new URL("./assets/pythonlogo.png", import.meta.url),
+    image: new URL("../assets/pythonlogo.png", import.meta.url),
     key: 5    
   }
 ]
